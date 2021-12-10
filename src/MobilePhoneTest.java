1d50d12e43cf20fbdde43aed85ba57daf9166798
Martin <m.vangenderen@ziggo.nl>
2021-12-10 16:42:54 +0100

    commit message

--- src/MobilePhoneTest.java
+++ src/MobilePhoneTest.java
@@ -1,4 +1,22 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class MobilePhoneTest {
-  
+
+    @Test
+    public void addNewContact() {
+    }
+
+    @Test
+    public void printContacts() {
+    }
+
+    @Test
+    public void queryContact() {
+    }
+
+    @Test
+    public void updateContact() {
+    }
 }
